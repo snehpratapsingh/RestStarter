2c52164d46ef9f30d427f2c8288403a033e168b9
Sneh <snehpratapsingh@gmail.com>
2019-08-12 11:39:56 +0530

    Initial commit

---
+++ RestStarter/src/main/java/com/spr/RestStarterApplication.java
@@ -0,0 +1,13 @@
+package com.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStarterApplication.class, args);
+	}
+
+}
---
+++ RestStarter/src/main/java/com/spr/resource/EmployeeResource.java
@@ -0,0 +1,5 @@
+package com.spr.resource;
+
+public class EmployeeResource {
+
+}
