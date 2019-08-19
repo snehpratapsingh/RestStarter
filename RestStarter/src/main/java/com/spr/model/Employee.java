4dfb7a3315c1ceddfa6e0e0a98c0385715167f13
spratap <spratap@AIPL-LT-566.altimetrik.com>
2019-08-19 17:00:56 +0530

    Added Logging and updated Entity PK

--- RestStarter/src/main/java/com/spr/model/Employee.java
+++ RestStarter/src/main/java/com/spr/model/Employee.java
@@ -1,12 +1,15 @@
 package com.spr.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @XmlRootElement
 public class Employee {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int empId;
 	private String empName;
 	public Employee(int empId, String empName) {
